0700fb2a609db7db0691c935677b31a487c75616
Gregory Peres <gregperes84@gmail.com>
2014-03-18 20:51:52 -0300

    adicionado documentacao

--- static/base/dispacher.js
+++ static/base/dispacher.js
@@ -3,12 +3,18 @@
 	"use strict";
 
 	/**
-	* 
+	* Classe que implementa o padrao PubSub
+	*
+	* @namespace Bizzy
+	* @class Dispacher
 	*/
 	function Dispacher () {}
 
 	/**
-	* 
+	* Propriedade que contem os callbacks que serao tratados pelo dispatcher
+	*
+	* @property _listeners
+	* @default []
 	*/
 	Object.defineProperty(Dispacher.prototype, "_listeners", {
 
@@ -18,7 +24,11 @@
 	});
 
 	/**
-	* 
+	* Recupera a lista de callbacks para um determinado evento
+	*
+	* @method _getEvent
+	* @param {String} event Nome do evento
+	* @return {Array} Lista de callbacks registrados para o evento
 	*/
 	Dispacher.prototype._getEvent = function (event) {
 
@@ -28,7 +38,12 @@
 	};
 
 	/**
-	* Subscribe
+	* Registra um novo callback de um determinado evento
+	*
+	* @method on
+	* @param {String} event Nome do evento
+	* @param {Function} callback Funcao de callback que a ser registrada
+	* @return {Void}
 	*/
 	Dispacher.prototype.on = function (event, callback) {
 
@@ -37,7 +52,12 @@
 	};
 
 	/**
-	* Delete
+	* Deleta um callback de um determinado evento
+	*
+	* @method off
+	* @param {String} event Nome do evento
+	* @param {Function} callback Funcao de callback que a ser registrada
+	* @return {Void}
 	*/
 	Dispacher.prototype.off = function (event, callback) {
 
@@ -60,7 +80,12 @@
 	};
 
 	/**
-	* Publish
+	* Dispara os callbacks de um determinado evento
+	*
+	* @method trigger
+	* @param {String} event Nome do evento
+	* @param {Object} data Objeto contendo os dados que serao passados para o callbach
+	* @return {Void}
 	*/
 	Dispacher.prototype.trigger = function (event, data) {
 
