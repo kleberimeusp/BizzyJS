dfbfa3a513a58f1f1b72ff95231f7c6b34916b92
Front-end Engineer <cleber.programmer@hotmail.com>
2014-04-01 11:48:36 -0300

    Refatoracao

--- src/models/rules/ruleFactory.js
+++ src/models/rules/ruleFactory.js
@@ -17,11 +17,18 @@
 	RuleFactory.prototype.create = function (defaults) {
 
 		var rule = new window.Bizzy.models.rules.Rule(),
-			name = "";
+			name = "",
+			i = 0;
 
 		for (name in defaults) {
 
-			rule = new this.__ruleMap[defaults[name].rule](rule);
+			i = defaults[name].validations.length;
+
+			while(--i) {
+
+				rule = new this.__ruleMap[defaults[name].validations[i].rule](rule);
+
+			}
 
 		}
 
@@ -42,6 +49,15 @@ var defaults = {
 		value: 0,
 
 		type: Number,
+
+		validations: [
+
+			{ rule: "lenght", value: 10, message: ""},
+			{ rule: "range", value: [0, 10], message: "" },
+			{ rule: "required", message: "" },
+			{ rule: "pattern", value: "email", message: "" }
+
+		],
 
 		range: [8, 90],
 
